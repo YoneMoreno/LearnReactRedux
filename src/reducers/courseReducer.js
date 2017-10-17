16c886e08118fa3d022f44d4d99df527d3d07003
YonePC <YonePC@YONE>
2017-10-17 16:07:06 +0100

    Updated app entry point index.js to dispatch the loadCourses action into the store.

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -4,7 +4,7 @@ export default function courseReducer(state = [], action){
   switch(action.type){
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
-    ];
+
 
     default:
       return state;
