cb732c0e143e73e274ce4426d827c6cb079ec419
YonePC <YonePC@YONE>
2017-10-22 09:28:34 +0100

    Added error handling to display a red pop up when course validation fails and updated internal redux store to display preloader just enough time to let user know processing is tried.

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,7 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
-import {beginAjaxCall} from "./ajaxStatusActions";
+import {ajaxCallError, beginAjaxCall} from "./ajaxStatusActions";
+
 
 export function loadCoursesSuccess(courses){
   return { type: types.LOAD_COURSES_SUCCESS, courses };
@@ -31,6 +32,7 @@ export function saveCourse(course){
       course.id ? dispatch(updateCourseSuccess(savedCourse)) :
         dispatch(createCourseSuccess(savedCourse));
     }).catch(error => {
+      dispatch(ajaxCallError(error));
       throw(error);
     });
   };
