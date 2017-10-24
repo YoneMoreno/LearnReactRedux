e9a5da025cdcd8ad14dbceb17b53dfb4b03bf7c5
YonePC <YonePC@YONE>
2017-10-24 18:29:33 +0100

    Green: save button is labeled "Save" when not saving

--- src/components/course/CourseForm.Enzyme.test.js
+++ src/components/course/CourseForm.Enzyme.test.js
@@ -21,7 +21,11 @@ describe('Enzyme tests', () =>{
     const wrapper = setup(false);
     expect(wrapper.find('form').length).toBe(1);
     expect(wrapper.find('h1').text()).toEqual('Manage Course');
+  });
 
+  it('save button is labeled "Save" when not saving', () =>{
+    const wrapper = setup(false);
+    expect(wrapper.find('input').props().value).toBe('Save');
   });
 });
 
