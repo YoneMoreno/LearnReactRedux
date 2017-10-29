87dcc194e6eb034d9b37c05f2dc58fb0f89215c6
YonePC <YonePC@YONE>
2017-10-29 08:51:52 +0000

    Trying to display an error page when user gets a 404 error.

---
+++ src/components/common/notFoundPage.js
@@ -0,0 +1,17 @@
+"use strict";
+let React = require('react');
+let Link = require('react-router').Link;
+
+let NotFoundPage = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <h1>Page Not Found</h1>
+        <p>Whoooops! Sorry, there is nothing to see here. :=)</p>
+        <p><Link to="app">Back to Home</Link></p>
+      </div>
+    );
+  }
+});
+
+module.exports = NotFoundPage;
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,10 @@ import AboutPage from './components/about/AboutPage';
 import { Route, IndexRoute } from 'react-router';
 import CoursesPage from './components/course/CoursesPage';
 import ManageCoursePage from './components/course/ManageCoursePage.'; //eslint-disable-line import/no-named-as-default
+
+let Router = require('react-router');
+let NotFoundRoute = Route.NotFoundRoute;
+
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
@@ -12,5 +16,6 @@ export default (
     <Route path="course" component={ManageCoursePage} />
     <Route path="course/:id" component={ManageCoursePage} />
     <Route path="about" component={AboutPage} />
+    <NotFoundRoute handler={require('./components/common/notFoundPage')} />
   </Route>
 );
